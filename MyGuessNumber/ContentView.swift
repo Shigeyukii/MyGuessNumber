f3dc744017461e8e38bfc3b20a100ec7aeb792be
SHIGEYUKI METOKI <shigeyuki.metoki@gmail.com>
2024-07-31 12:45:28 +0900

    Initial Commit

---
+++ MyGuessNumber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGuessNumber
+//
+//  Created by user on 2024/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGuessNumber/MyGuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGuessNumberApp.swift
+//  MyGuessNumber
+//
+//  Created by user on 2024/07/31.
+//
+
+import SwiftUI
+
+@main
+struct MyGuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
